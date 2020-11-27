07bcd5098b0cb65fdc10a6a890e8ce5152ff1317
Giedrius Mėčius <giedriusmecius@gmail.com>
2020-11-27 10:46:43 +0100

    added more elements to hotel screen

--- lib/screens/hotel_screen.dart
+++ lib/screens/hotel_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_travel_ui_starter/models/hotel_model.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class HotelScreen extends StatefulWidget {
   final Hotel hotel;
@@ -32,6 +33,7 @@ class _HotelScreenState extends State<HotelScreen> {
                 child: Hero(
                   tag: widget.hotel.imageUrl,
                   child: ClipRRect(
+                    borderRadius: BorderRadius.circular(20),
                     child: Image(
                       image: AssetImage(
                         widget.hotel.imageUrl,
@@ -41,8 +43,105 @@ class _HotelScreenState extends State<HotelScreen> {
                   ),
                 ),
               ),
-              Text(widget.hotel.name),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 40, horizontal: 10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    IconButton(
+                      onPressed: () => Navigator.pop(context),
+                      icon: Icon(
+                        Icons.arrow_back,
+                        size: 35,
+                        color: Colors.black,
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        Icon(
+                          Icons.search,
+                          size: 35,
+                          color: Colors.black,
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Icon(
+                          FontAwesomeIcons.sortAmountDown,
+                          size: 35,
+                          color: Colors.black,
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
             ],
+          ),
+          Expanded(
+            child: SingleChildScrollView(
+              scrollDirection: Axis.vertical,
+              physics: AlwaysScrollableScrollPhysics(),
+              child: Container(
+                margin: EdgeInsets.fromLTRB(10, 15, 10, 5),
+                height: 450,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black26,
+                      blurRadius: 6,
+                      offset: Offset(0.0, 2.0),
+                    )
+                  ],
+                ),
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 30, vertical: 20),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            widget.hotel.name,
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                          Text(
+                            '\$${widget.hotel.price}',
+                            style: TextStyle(
+                                fontSize: 22, fontWeight: FontWeight.w600),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(0, 0, 210, 0),
+                      child: Text(
+                        'Description:',
+                        style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.w600,
+                            color: Colors.grey),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(widget.hotel.address),
+                  ],
+                ),
+              ),
+            ),
           ),
         ],
       ),
